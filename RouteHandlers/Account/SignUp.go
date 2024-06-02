1547500f50126066276b09d69e91a3ea77fdb1f5
Jacob <69864916+Jacobinoo@users.noreply.github.com>
2024-06-02 18:01:51 +0200

    registration

--- RouteHandlers/Account/SignUp.go
+++ RouteHandlers/Account/SignUp.go
@@ -5,7 +5,6 @@ import (
 	"ConnectServer/Types"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"unicode/utf8"
@@ -18,54 +17,53 @@ func SignUpHandler(writer http.ResponseWriter, request *http.Request) {
 
 	var accountToRegister Types.AccountRegisterData
 
-	err := Helpers.DecodeJSONBody(writer, request, &accountToRegister)
-	if err != nil {
+	decodingError := Helpers.DecodeJSONBody(writer, request, &accountToRegister)
+	if decodingError != nil {
 		var malformedReq *Helpers.MalformedRequest
-		if errors.As(err, &malformedReq) {
+		if errors.As(decodingError, &malformedReq) {
 			http.Error(writer, malformedReq.Msg, malformedReq.Status)
 		} else {
-			log.Print(err.Error())
+			log.Print(decodingError.Error())
 			http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		}
 		return
 	}
 
-	registerDataValid, err := validateRegistrationFormData(&accountToRegister)
-	fmt.Println(registerDataValid)
-	if err != nil {
-		log.Println(err)
+	validationError := validateRegistrationFormData(&accountToRegister)
+	if validationError != nil {
+		log.Println(validationError)
 		switch {
-		case errors.Is(err, Helpers.ErrSignUpInvalidEmail):
+		case errors.Is(validationError, Helpers.ErrSignUpInvalidEmail):
 			http.Error(writer, "Provided email is invalid", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordTooLong):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordTooLong):
 			http.Error(writer, "Password is too long", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordTooShort):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordTooShort):
 			http.Error(writer, "Password is too short", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordNoDigit):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordNoDigit):
 			http.Error(writer, "Password does not contain a digit", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordNoLower):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordNoLower):
 			http.Error(writer, "Password does not contain a lowercase letter", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordNoUpper):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordNoUpper):
 			http.Error(writer, "Password does not contain an uppercase letter", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordNoSpecial):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordNoSpecial):
 			http.Error(writer, "Password does not contain a special character", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpPasswordMismatch):
+		case errors.Is(validationError, Helpers.ErrSignUpPasswordMismatch):
 			http.Error(writer, "Provided password and confirm password are different", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpInvalidFirstName):
+		case errors.Is(validationError, Helpers.ErrSignUpInvalidFirstName):
 			http.Error(writer, "Provided first name is invalid", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpInvalidLastName):
+		case errors.Is(validationError, Helpers.ErrSignUpInvalidLastName):
 			http.Error(writer, "Provided last name is invalid", http.StatusBadRequest)
 			return
-		case errors.Is(err, Helpers.ErrSignUpNameContainsWhitespace):
+		case errors.Is(validationError, Helpers.ErrSignUpNameContainsWhitespace):
 			http.Error(writer, "Names can't contain whitespaces", http.StatusBadRequest)
 			return
 		}
@@ -92,31 +90,31 @@ func SignUpHandler(writer http.ResponseWriter, request *http.Request) {
 
 }
 
-func validateRegistrationFormData(registerFormData *Types.AccountRegisterData) (formDataValid bool, error error) {
+func validateRegistrationFormData(registerFormData *Types.AccountRegisterData) error {
 	if !Helpers.EmailRegexp.MatchString(registerFormData.Email) {
-		return false, Helpers.ErrSignUpInvalidEmail
+		return Helpers.ErrSignUpInvalidEmail
 	}
 
 	_, err := Helpers.PasswordValid(registerFormData.Password)
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	if registerFormData.Password != registerFormData.ConfirmPassword {
-		return false, Helpers.ErrSignUpPasswordMismatch
+		return Helpers.ErrSignUpPasswordMismatch
 	}
 
 	if utf8.RuneCountInString(registerFormData.FirstName) > 35 || utf8.RuneCountInString(registerFormData.FirstName) <= 0 {
-		return false, Helpers.ErrSignUpInvalidFirstName
+		return Helpers.ErrSignUpInvalidFirstName
 	}
 
 	if utf8.RuneCountInString(registerFormData.LastName) > 35 || utf8.RuneCountInString(registerFormData.LastName) <= 0 {
-		return false, Helpers.ErrSignUpInvalidLastName
+		return Helpers.ErrSignUpInvalidLastName
 	}
 
 	if Helpers.ContainsWhitespaceRegexp.MatchString(registerFormData.FirstName) || Helpers.ContainsWhitespaceRegexp.MatchString(registerFormData.LastName) {
-		return false, Helpers.ErrSignUpNameContainsWhitespace
+		return Helpers.ErrSignUpNameContainsWhitespace
 	}
 
-	return true, nil
+	return nil
 }
